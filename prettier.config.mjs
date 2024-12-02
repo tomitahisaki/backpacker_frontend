6e28daf60f0f716a89979fa62b12052530b67c55
hisakey <alltimelow2011@gmail.com>
2024-12-02 09:15:11 +0900

    fix: prettier setting

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,22 +1,24 @@
 /** @type {import("prettier").Config} */
 const config = {
-    tabWidth: 4,
-    printWidth: 120,
-    overrides: [
-        {
-            files: "*.{json,json5,html,yaml,yml}",
-            options: {
-                tabWidth: 2,
-            },
-        },
-        {
-            files: "*.md",
-            options: {
-                tabWidth: 2,
-                trailingComma: "none",
-            },
-        },
-    ],
+  tabWidth: 2,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  overrides: [
+    {
+      files: '*.{json,json5,html,yaml,yml}',
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        tabWidth: 2,
+        trailingComma: 'none',
+      },
+    },
+  ],
 };
 
 export default config;
