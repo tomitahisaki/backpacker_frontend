ee122d7b3f38daa4433899dc6cf92ae9fadb8ad1
hisakey <alltimelow2011@gmail.com>
2024-12-14 16:48:22 +0900

    feat: add eslint prettier

--- api/client.ts
+++ api/client.ts
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const client = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: 'http://localhost:3000/api/v1',
   timeout: 5000,
 });
 
 export default client;
-
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
-import { getHealthCheck } from "../api/healthCheck";
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import { getHealthCheck } from '../api/healthCheck';
 
 export default function Index() {
-  const [responseText, setResponseText] = useState("Loading..."); // 初期値を設定
+  const [responseText, setResponseText] = useState('Loading...'); // 初期値を設定
 
   useEffect(() => {
     (async () => {
       try {
-        const data = await getHealthCheck() as { message: string }; // API呼び出し
-        setResponseText(data.message || "Unknown Status"); // レスポンスをstateに保存
+        const data = (await getHealthCheck()) as { message: string }; // API呼び出し
+        setResponseText(data.message || 'Unknown Status'); // レスポンスをstateに保存
       } catch (error) {
-        console.error("Error fetching health check:", error);
-        setResponseText("Error"); // エラーメッセージ
+        console.error('Error fetching health check:', error);
+        setResponseText('Error'); // エラーメッセージ
       }
     })();
   }, []); // 初回レンダリング時に一度だけ実行
@@ -21,13 +21,11 @@ export default function Index() {
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <Text style={{ fontSize: 18 }}>Health Check Result:</Text>
       <Text style={{ fontSize: 24, marginTop: 10 }}>{responseText}</Text>
     </View>
   );
 }
-
