d4952ba7503b9af1717d0e0fb1a7c8dfa55d65d7
hisakey <alltimelow2011@gmail.com>
2025-01-09 09:41:09 +0900

    feat: header icon is defined

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,9 +10,8 @@ const Header = () => {
 
   return (
     <Appbar.Header style={styles.header}>
-      {/* <Appbar.Action icon="compass" onPress={() => {}} /> */}
       <TouchableOpacity onPress={() => {}} style={styles.iconContainer}>
-        <Image source={require('@/assets/images/rounded-header-icon.png')} style={styles.icon} />
+        <Image source={require('@/assets/images/tabi-portal-header.webp')} style={styles.icon} />
       </TouchableOpacity>
       <Appbar.Content title="TabiPortal" titleStyle={styles.title} />
       <Appbar.Action icon="menu" onPress={handleMenuPress} />
@@ -22,14 +21,15 @@ const Header = () => {
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: '#C8E6C9', // ヘッダーの背景色（好みに応じて変更可能）
+    backgroundColor: '#B7CEB5', // ヘッダーの背景色（好みに応じて変更可能）
   },
   iconContainer: {
     padding: 10,
   },
   icon: {
-    width: 24,
-    height: 24,
+    width: 40,
+    height: 40,
+    borderRadius: 5, // アイコンの形状を丸くする
   },
   title: {
     fontSize: 20,
